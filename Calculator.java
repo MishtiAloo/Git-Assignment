53951f85d7495abc5921cbbffe09081402673ed6
f.tahmid123@gmail.com <f.tahmid123@gmail.com>
2025-08-20 09:05:57 +0600

    Changed divide method in main branch

---
+++ Calculator.java
@@ -0,0 +1,27 @@
+
+public class Calculator {
+
+    // Method to add two numbers
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    // Method to subtract two numbers
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    // Method to multiply two numbers
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    // Method to divide two numbers
+    public double divide(int a, int b) {
+        if (b == 0) {
+            System.out.println("Error: Division by zero is not allowed.");
+        }
+        else return (double) a / b;
+    }
+
+}
